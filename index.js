809f42e051caa5a6cedcb0e87cbb3e4f8a7452e6
JokerYRF <276044311@qq.com>
2019-11-27 09:37:15 +0800

    no message

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ const gas = require('./gas.js') //引入气体扩散模块
 
 var bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: false }));//解析字符
-var allowCors = function (req, res, next) {
+app.use(bodyParser.urlencoded({
+    extended: false
+})); //解析字符
+var allowCors = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', req.headers.origin);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.header('Access-Control-Allow-Credentials', 'true');
     next();
 };
-app.use(allowCors);//使用跨域中间件
+app.use(allowCors); //使用跨域中间件
 
 
 
